5e43c9a4ae109551a3187ab095b28c891a7742ba
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-07-23 03:11:04 +0530

    i am back

--- gui/main_file.py
+++ gui/main_file.py
@@ -1,76 +1,78 @@
 import PySimpleGUI as sg
 import mysql.connector as sql
 
+
 class GUI:
     def __init__(self):
-        self.main_window=None
+        self.main_window = None
 
         self.admin_layout = [
             [sg.Text('Admin Login')],
             [sg.Text('Username: '), sg.Input(key='Username')],
-            [sg.Text('Password: '), sg.Input(key='Password',password_char='*')],
-            [sg.Button('Login'),sg.Button('Exit')]]
+            [sg.Text('Password: '), sg.Input(key='Password', password_char='*')],
+            [sg.Button('Login'), sg.Button('Exit')]]
 
-        self.account_layout =[
+        self.account_layout = [
             [sg.Text('Account Name:')],
-            [sg.Text('Account Number:'),sg.Input(key='Number')],
-            [sg.Text('OTP:'),sg.Input(key='OTP')],
-            [sg.Button('Login'),sg.Button('Exit')]]
+            [sg.Text('Account Number:'), sg.Input(key='Number')],
+            [sg.Text('OTP:'), sg.Input(key='OTP')],
+            [sg.Button('Login'), sg.Button('Exit')]]
 
-        self.layout = [[sg.Text('Welcome to STATE BANK OF INDIA'),sg.Column(self.admin_layout, visible=False,key='-COL1-'), sg.Column(self.account_layout, visible=False,key='-COL2-')],
+        self.layout = [
+            [sg.Text('Welcome to STATE BANK OF INDIA'), sg.Column(self.admin_layout, visible=False, key='-COL1-'),
+             sg.Column(self.account_layout, visible=False, key='-COL2-')],
             [sg.Button('Admin'), sg.Button('Account'), sg.Button('Exit')]]
 
-   
     def run(self):
-            self.main_window = sg.Window('Introduction',self.layout,finalize=True)
-    
-            while True:
-                #database
-                conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
-                cur = conn.cursor()
-                query = "SELECT username, passwordx FROM admin"
-                cur.execute(query)
-                results = cur.fetchall()
+        self.main_window = sg.Window('Introduction', self.layout, finalize=True)
 
-                #gui window
-                event, values = self.main_window.read()
-                print(event, values)
+        while True:
+            # database
+            conn = sql.connect(host='localhost', user='Lakshay', passwd='lakshay*91250538', database='BANK_DBMS')
+            cur = conn.cursor()
+            query = "SELECT username, passwordx FROM admin"
+            cur.execute(query)
+            results = cur.fetchall()
+
+            # gui window
+            event, values = self.main_window.read()
+            print(event, values)
+            if event in (None, 'Exit'):
+                break
+
+            if event == 'Admin':
+                self.main_window['-COL1-'].update(visible=True)
                 if event in (None, 'Exit'):
                     break
-                
-                if event == 'Admin':
-                    self.main_window['-COL1-'].update(visible=True)
-                    if event in (None, 'Exit'):
-                        break
-                    elif event == 'Login':
-                        self.username = str(values['-USERNAME-'])
-                        self.password = str(values['-PASSWORD-'])
-                        authenticated = False  # Initialize the variable here
-                        for row in results:
-                            username = row[0]
-                            passwordx = row[1]
-                    # Add your authentication logic here
-                            if username == row[0] and password == row[1]:
-                                authenticated = True
-                                break
-                                #authenticated = True
-                        #if authenticated:
-                            #popup window for login authentication
+                elif event == 'Login':
+                    self.username = str(values['-USERNAME-'])
+                    self.password = str(values['-PASSWORD-'])
+                    authenticated = False  # Initialize the variable here
+                    for row in results:
+                        username = row[0]
+                        passwordx = row[1]
+                        # Add your authentication logic here
+                        if self.username == row[0] and self.password == row[1]:
+                            authenticated = True
+                            break
+                            # authenticated = True
+                    # if authenticated:
+                    # popup window for login authentication
 
-                
-                elif event == 'Account':
-                    self.main_window['-COL2-'].update(visible=True)
-                    if event in (None, 'Exit'):
-                        break
-                    elif event == 'Login':
-                        self.Account_Number = str(values['-Account Number-'])
-                        self.OTP = str(values['-OTP-'])
+
+            elif event == 'Account':
+                self.main_window['-COL2-'].update(visible=True)
+                if event in (None, 'Exit'):
+                    break
+                elif event == 'Login':
+                    self.Account_Number = str(values['-Account Number-'])
+                    self.OTP = str(values['-OTP-'])
                     # Add your authentication logic here
-                        if self.Account_Number == 'admin' and self.OTP == 'admin':
-                            sg.popup('Login Successful!')
-                        else:
-                            sg.popup('Invalid Credentials!')
-            self.main_window.close()
+                    if self.Account_Number == 'admin' and self.OTP == 'admin':
+                        sg.popup('Login Successful!')
+                    else:
+                        sg.popup('Invalid Credentials!')
+        self.main_window.close()
 
 
 if __name__ == '__main__':
