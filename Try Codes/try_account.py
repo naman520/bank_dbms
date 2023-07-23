84d3925579a0b0c94ae8d07bb5b98c817a096d43
Naman Gupta <namangupta2360@gmail.com>
2023-07-23 13:49:41 +0530

    customer authentication

--- Try Codes/try_account.py
+++ Try Codes/try_account.py
@@ -3,14 +3,15 @@ import mysql.connector as sql
 
 conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
 cur = conn.cursor()
-query = "SELECT acc_no FROM user"
+query = "SELECT * FROM customer_login"
 cur.execute(query)
 results = cur.fetchall()
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Account Holder'),sg.InputText()],
-            [sg.Text('Password'),],
+layout = [  [sg.Text('Account Holder Name'),sg.InputText()],
+            [sg.Text('Account Number'),sg.InputText()],
+            [sg.Text('Pin'),sg.InputText()],
             [sg.Button('Login'), sg.Button('Cancel')] ]
 
 # Create the Window
@@ -20,7 +21,7 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    if (values[0]) in results:
+    if (values[0],values[1],values[2]) in results:
         print("Authentication successful")
     else:
         print("Authentication failed")
