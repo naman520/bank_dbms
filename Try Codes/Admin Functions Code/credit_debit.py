8720e4afe0724aa5345fd75eec7e61b5b3a54fa2
Naman Gupta <namangupta2360@gmail.com>
2023-07-31 22:04:30 +0530

    credit_debit changes

--- Try Codes/Admin Functions Code/credit_debit.py
+++ Try Codes/Admin Functions Code/credit_debit.py
@@ -1,25 +1,24 @@
 import PySimpleGUI as sg
 import mysql.connector as sql
 
+conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
+cur = conn.cursor()
+
 def amt():
-    conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
-    cur = conn.cursor()
     amt = "SELECT money FROM customer_login"
     cur.execute(amt)
-    results = cur.fetchall()
+    return 
 
-def credit(acc_no,name,amount):
+def credit(acc_no,amount):
     #database connection
-    conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
-    cur = conn.cursor()
+    
     query = "SELECT * FROM customer_login"
     cur.execute(query)
     results = cur.fetchall()
 
     #gui
     sg.theme('LightBlue5')
-    layout = [  [sg.Text('Account Holder Name'),sg.InputText()],
-                [sg.Text('Account Number'),sg.InputText()],
+    layout = [  [sg.Text('Account Number'),sg.InputText()],
                 [sg.Button('Search'), sg.Button('Cancel')] ]
     layout2 =[ [sg.Text('Amount to be Credited'),sg.InputText()],
                 [sg.Button('Credit Amount'), sg.Button('Cancel Transaction')]]
@@ -30,16 +29,15 @@ def credit(acc_no,name,amount):
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
-        if (values[0] and values[1]) in results:
+        if (values[1]) in results[1]:
             window = sg.Window('Credit',layout2)
             while True:
                 event, values = window.read()
                 if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
                     break
                 else:
-                    
-
+                    money_query = "UPDATE customer_login SET money ="+ str(amount+original) +" where acc_no = "+str(acc_no)
 
     window.close()
 
-credit(1596,'john',2000)
+credit(1598,2000)
