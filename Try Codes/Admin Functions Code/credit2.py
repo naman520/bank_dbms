267c3d1e7c48021ac85223ab02bf1b568eb68632
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-08-04 01:16:46 +0530

    Credit done

--- Try Codes/Admin Functions Code/credit2.py
+++ Try Codes/Admin Functions Code/credit2.py
@@ -3,9 +3,6 @@ import mysql.connector as sql
 
 conn=sql.connect(host='localhost',user='root',passwd='123456',database='BANK_DBMS')
 cur = conn.cursor()
-query = "SELECT acc_no FROM customer_login"
-cur.execute(query)
-results = cur.fetchall()
 
 sg.theme("LightBlue5")
 layout = [[sg.Text('Account Number'),sg.InputText()],
@@ -17,17 +14,17 @@ layout3 = [[layout,sg.Column(layout2,visible=False,key='-Col-')]]
 window = sg.Window('Admin',layout3)
 while True:
     event, values = window.read()
-    values = tuple(values.values())
-    values = values[0]
-    print(values, results)
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-    if event == 'search':
+    if event == 'Search':
         account_number = values[0]
-        if (account_number) in results:
+        try:
+            query = "SELECT acc_no FROM customer_login where acc_no = " + str(account_number)
+            cur.execute(query)
+            results = cur.fetchall()
             print("Account found:", account_number)
-            window['-Col2-'].update(visible=True)
-        else:
+            window['-Col-'].update(visible=True)
+        except:
             print("Account not found:", account_number)
             sg.popup("Account not found. Please enter a valid account number.")
 
