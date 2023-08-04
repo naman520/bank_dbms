5122b778996e75626c354182f49bc477e9ca99ae
Naman Gupta <namangupta2360@gmail.com>
2023-08-04 13:14:56 +0530

    credit function created successfully

--- Try Codes/Admin Functions Code/credit2.py
+++ Try Codes/Admin Functions Code/credit2.py
@@ -7,13 +7,16 @@ cur = conn.cursor()
 sg.theme("LightBlue5")
 layout = [[sg.Text('Account Number'),sg.InputText()],
         [sg.Button('Search'), sg.Button('Cancel')]]
+
 layout2 = [ [sg.Text('amount'),sg.InputText()],
         [sg.Button('Credit Amount'), sg.Button('Cancel')]]
 
 layout3 = [[layout,sg.Column(layout2,visible=False,key='-Col-')]]
 window = sg.Window('Admin',layout3)
+
 while True:
     event, values = window.read()
+
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
     if event == 'Search':
@@ -23,20 +26,24 @@ while True:
             cur.execute(query)
             results = cur.fetchall()
             print("Account found:", account_number)
+
             window['-Col-'].update(visible=True)
         except:
             print("Account not found:", account_number)
             sg.popup("Account not found. Please enter a valid account number.")
+
     if event == 'Credit Amount':
         if event == sg.WIN_CLOSED or event == 'Cancel':
             break
-        #amount = values[3]
         try:
             print(values)
+
             get_money_query = "SELECT money FROM customer_login where acc_no = " + str(account_number)
             print(get_money_query)
             cur.execute(get_money_query)
+
             amount = cur.fetchall()[0][0]
+
             n = int(values[1])
             print(amount)
             money_query = "UPDATE customer_login SET money ="+ str(amount+n) +" where acc_no = "+str(account_number)
@@ -49,5 +56,4 @@ while True:
             sg.popup("Transaction Failed")
             
 conn.commit()
-
 window.close()
